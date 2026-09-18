f9ebe0777d75d5bdd7dd133bff73333ad1c7222e
Your Name <you@example.com>
2026-09-18 10:35:59 -0300

    fix: return auth URL as a service value object

--- controllers/front/auth.php
+++ controllers/front/auth.php
@@ -6,7 +6,8 @@ class agblingauthModuleFrontController extends ModuleFrontController
         AgClienteLogger::createLogger(_PS_MODULE_DIR_ . 'agbling/logs/auth.log', 1);
         AgClienteLogger::addLog("Iniciando autenticação com o Bling.");
         
-        Tools::redirect($this->get(AGTI\Bling\ValueObject\ApiAuthUrl::class)  . '?redirect_back=' . urlencode($this->context->link->getModuleLink('agbling', 'receiveToken')));
+        $apiAuthUrl = $this->get(AGTI\Bling\ValueObject\ApiAuthUrl::class);
+        Tools::redirect($apiAuthUrl->getUrl() . '?redirect_back=' . urlencode($this->context->link->getModuleLink('agbling', 'receiveToken')));
 
         exit();
     }
--- src/Infrastructure/Factory/ApiAuthUrl.php
+++ src/Infrastructure/Factory/ApiAuthUrl.php
@@ -2,14 +2,15 @@
 namespace AGTI\Bling\Infrastructure\Factory;
 
 use AGTI\Bling\ValueObject\ApiToken;
+use AGTI\Bling\ValueObject\ApiAuthUrl as ApiAuthUrlValueObject;
 use AGTI\Bling\ValueObject\Configuration as VBConfiguration;
 use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiAuthUrl
 {
-    public static function getUrl()
+    public static function getUrl(): ApiAuthUrlValueObject
     {
-        return "https://www.agti.eng.br/module/agblingintermediator/auth";
+        return (new ApiAuthUrlValueObject())->setUrl("https://www.agti.eng.br/module/agblingintermediator/auth");
     }
 }
